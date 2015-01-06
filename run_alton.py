37e3088e1c129e23c5c39ca4557edd3c11f46eb1
Feanil Patel <feanil@edx.org>
2015-01-06 17:00:12 -0500

    Upgrade alton to use the latest will.

--- run_alton.py
+++ run_alton.py
@@ -2,6 +2,6 @@
 from will.main import WillBot
 
 if __name__ == '__main__':
-    bot = WillBot(plugins_dirs=["plugins",], template_dirs=["templates",])
+    bot = WillBot()
     bot.bootstrap()
     
