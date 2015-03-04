0fa70c3df4a9e4dc1cb58d32bb0c8027f6b7277c
Fred Smith <fred.smith@fredsmith.org>
2015-03-04 15:25:03 -0500

    bugfixes, documentation

--- plugins/user.py
+++ plugins/user.py
@@ -8,18 +8,29 @@ from boto.s3.key import Key
 import pprint
 import cStringIO
 
-class UserPlugin(WillPlugin):
-    def check_user_permission(self, username, permission):
-        user_permissions = self.load("user_permissions", {})
+def check_user_permission(storage, username, permission):
+    user_permissions = storage.load("user_permissions", {})
 
-        if username in user_permissions:
-            if permission in user_permissions[username]:
-                return 1
-            else:
-                return 0
+    if username in user_permissions:
+        if permission in user_permissions[username]:
+            return 1
         else:
             return 0
+    else:
+        return 0
 
+def verify_twofactor(storage, username, secret):
+    user_twofactor = storage.load("user_twofactor", {})
+    if username in user_twofactor:
+        totp = pyotp.TOTP(user_twofactor[username])
+        if totp.verify(secret):
+            return 1
+        else:
+            return 0
+    else:
+        return 0
+
+class UserPlugin(WillPlugin):
     def generate_and_upload_QR(self,secret,username):
         principle = 'devops@edx.org'
         issuer = 'edx-ops'
@@ -36,53 +47,55 @@ class UserPlugin(WillPlugin):
         return url
 
     @respond_to("^twofactor verify (?P<secret>\w*)")
-    def verify_twofactor(self, message, secret):
-        user_twofactor = self.load("user_twofactor", {})
+    def verify_user_twofactor(self, message, secret):
+        """
+        twofactor verify [secret]: verify your twofactor authentication system
+        """
         username = message.sender.nick
-        if username in user_twofactor:
-            totp = pyotp.TOTP(user_twofactor[username])
-            if totp.verify(secret):
-                self.reply(message, "You are authenticated, {}".format(username)) 
-            else:
-                self.reply(message, "That's not correct,{}".format(username))
-
+        if verify_twofactor(self,username, secret):
+            self.reply(message, "You are authenticated, {}".format(username)) 
         else:
-            self.reply(message, "twofactor auth not set up for {}".format(username)) 
+            self.reply(message, "That's not correct,{}".format(username))
 
-
-    @respond_to("^debug messageobject")
-    def debug_messageobject(self, message):
-        pp = pprint.PrettyPrinter(indent=4)
-        message['type'] = 'chat'
-        self.reply(message, pp.pformat(message))
 
     @respond_to("^twofactor me")
     def create_user_twofactor(self, message):
+        """
+        twofactor me: set up twofactor authentication for your user
+        """
         username = message.sender.nick
         user_twofactor = self.load("user_twofactor", {})
         if username not in user_twofactor:
             user_twofactor[username] = pyotp.random_base32()
             message['type'] = 'chat'
-            #self.reply(message, "twofactor is: {}".format(user_twofactor[username])) 
             self.reply(message, self.generate_and_upload_QR(user_twofactor[username], username))
             self.save("user_twofactor", user_twofactor)
+            self.reply(message, "say 'twofactor verify <secret key>' to check your twofactor verification")
         else:
             message['type'] = 'chat'
             self.reply(message, "twofactor is already set up!") 
 
     @respond_to("^twofactor remove (?P<username>\w*)")
     def remove_user_twofactor(self, message, username):
-        user_twofactor = self.load("user_twofactor", {})
-        if username in user_twofactor:
-            del user_twofactor[username]
-            self.reply(message, "twofactor is removed for {}".format(username)) 
-            self.save("user_twofactor", user_twofactor)
+        """
+        twofactor remove [username]: remove [username]'s twofactor authentication (requires 'admin' permission)
+        """
+        if check_user_permission(self, message.sender.nick, 'admin'):  
+            user_twofactor = self.load("user_twofactor", {})
+            if username in user_twofactor:
+                del user_twofactor[username]
+                self.reply(message, "twofactor is removed for {}".format(username)) 
+                self.save("user_twofactor", user_twofactor)
+            else:
+                self.reply(message, "twofactor was not set for {}".format(username)) 
         else:
-            self.reply(message, "twofactor was not set for {}".format(username)) 
-
+            self.say("@{}: you don't have admin permission".format(message.sender.nick), message=message) 
 
     @respond_to("^what can (?P<username>\w*) do")
     def show_user_permission(self, message, username):
+        """
+        what can [username] do?: get someone's permissions
+        """
         if 'I' == username:
             username = message.sender.nick
         user_permissions = self.load("user_permissions", {})
@@ -94,6 +107,9 @@ class UserPlugin(WillPlugin):
 
     @respond_to("^who can(?P<permissions>( \w*)*)")
     def find_user_permission(self, message, permissions):
+        """
+        who can [permission]?: find the list of people with a permission
+        """
         user_permissions = self.load("user_permissions", {})
         for permission in permissions.split():
             userlist = []
@@ -106,21 +122,23 @@ class UserPlugin(WillPlugin):
 
     @respond_to("^can I(?P<permissions>( \w*)*)")
     def confirm_user_permission(self, message, permissions):
-        user_permissions = self.load("user_permissions", {})
+        """
+        can I [permission]?: check if you have a specific permission
+        """
         for permission in permissions.split():
-            try:
-                if permission in user_permissions[message.sender.nick]:
-                    self.say("@{}: Yes, you can {}".format(message.sender.nick, permission), message=message)
-                else:
-                    self.say("@{}: No, you can't {}".format(message.sender.nick, permission), message=message)
-            except KeyError:
+            if check_user_permission(self, message.sender.nick, 'grant'):  
+                self.say("@{}: Yes, you can {}".format(message.sender.nick, permission), message=message)
+            else:
                 self.say("@{}: No, you can't {}".format(message.sender.nick, permission), message=message)
 
 
 
     @respond_to("^give (?P<username>\w*) permission(?P<permissions>( \w*)*)")
     def give_user_permission(self, message, username, permissions):
-        if self.check_user_permission(message.sender.nick, 'grant'):  
+        """
+        give [username] permission to [permission]: grant a user a permission (requires 'grant' permission)
+        """
+        if check_user_permission(self, message.sender.nick, 'grant'):  
             requested_permissions = permissions.split()
             try:
                 requested_permissions.remove("to")
@@ -141,21 +159,27 @@ class UserPlugin(WillPlugin):
 
     @respond_to("^take away from (?P<username>\w*) permission(?P<permissions>( \w*)*)")
     def remove_user_permission(self, message, username, permissions):
-        requested_permissions = permissions.split()
-        requested_permissions.remove("to")
-        #self.say("removing permissions : {} ".format(', '.join(requested_permissions)), message=message) 
-        try:
-            user_permissions = self.load("user_permissions", {})
-            for permission in requested_permissions:
-                try:
-                    user_permissions[username].remove(permission)
-                except ValueError:
-                    pass
-        except KeyError:
-            pass
-        self.save("user_permissions", user_permissions)
-        self.say("@{}: new permissions for {} are: {} ".format(message.sender.nick, username, ', '.join(user_permissions[username])), message=message) 
+        """
+        take away from [username] permission [permission]: remove someone's permission (requires grant permission)
+        """
+        if check_user_permission(self, message.sender.nick, 'grant'):  
+            requested_permissions = permissions.split()
+            try:
+                requested_permissions.remove("to")
+            except ValueError:
+                pass
+            #self.say("removing permissions : {} ".format(', '.join(requested_permissions)), message=message) 
+            try:
+                user_permissions = self.load("user_permissions", {})
+                for permission in requested_permissions:
+                    try:
+                        user_permissions[username].remove(permission)
+                    except ValueError:
+                        pass
+            except KeyError:
+                pass
+            self.save("user_permissions", user_permissions)
+            self.say("@{}: new permissions for {} are: {} ".format(message.sender.nick, username, ', '.join(user_permissions[username])), message=message) 
+        else:
+            self.say("@{}: you don't have grant permission".format(message.sender.nick), message=message) 
 
-    @respond_to("^trash all permissions")
-    def delete_all_permission(self, message):
-        self.save("user_permissions", {})
